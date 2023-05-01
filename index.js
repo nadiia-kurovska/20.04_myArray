e6a0fa44b8daccbd17eda386a2fb5871dcda1254
Nadia Kurovska <nadiazabara@gmail.com>
2023-05-01 07:47:27 +0300

    add shift and unshift

---
+++ index.js
@@ -0,0 +1,43 @@
+function MyArray() {
+    this.length = 0;
+}
+
+function MyArrayProto(){
+    this.push = function (newElem){
+        this[this.length] = newElem;
+        this.length++;
+        return this.length;
+    };
+
+    this.pop = function () {
+
+        if(this.length > 0){
+            const lastItem = this[this.length -1];
+            delete this[--this.length];
+            return lastItem;
+        }
+        return undefined;
+    };
+
+    this.unshift = function(newElem){
+        this[0] = newElem;
+        this.length++;
+        return this.length;
+    };//how to save the first item of the initial array?
+
+    this.shift = function(){
+        if(this.length > 0) {
+            const firstItem = this[0];
+            delete this[0];
+            this.length--;
+            return firstItem;
+        }
+        return undefined;
+    };
+}
+
+MyArray.prototype = new MyArrayProto();
+
+const myArr1 = new MyArray();
+
+const arr = [];
