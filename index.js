772f1b840305b48ad3e08108d9508aaf84cbb0f1
Nadia Kurovska <nadiazabara@gmail.com>
2023-05-08 16:42:13 +0300

    change to class

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-function MyArray() {
-    this.length = 0;
-}
+// function MyArray() {
+//     this.length = 0;
+// }
+class MyArray{
+    constructor(){
+        this.length = 0;
+    }
 
-function MyArrayProto(){
-    this.push = function (newElem){
+    push(newElem){
         this[this.length] = newElem;
         this.length++;
         return this.length;
-    };
+    }
 
-    this.pop = function () {
-
+    pop() {
         if(this.length > 0){
             const lastItem = this[this.length -1];
             delete this[--this.length];
             return lastItem;
         }
         return undefined;
-    };
+    }
 
-    this.unshift = function(newElem){
-        this[0] = newElem;
-        this.length++;
-        return this.length;
-    };//how to save the first item of the initial array?
-
-    this.shift = function(){
+    shift(){
         if(this.length > 0) {
             const firstItem = this[0];
             delete this[0];
@@ -33,10 +29,61 @@ function MyArrayProto(){
             return firstItem;
         }
         return undefined;
-    };
+    }
+
+    unshift(newElem) {
+        let i = this[0]
+        this[i] = this[i++];//not working yet
+        this[0] = newElem;
+        this.length++;
+        return this.length;
+    }
+
+    forEach(addOne){
+        const addOne = function {
+            
+        }
+    }
+
 }
 
-MyArray.prototype = new MyArrayProto();
+
+
+// function MyArrayProto(){
+//     this.push = function (newElem){
+//         this[this.length] = newElem;
+//         this.length++;
+//         return this.length;
+//     };
+
+//     this.pop = function () {
+
+//         if(this.length > 0){
+//             const lastItem = this[this.length -1];
+//             delete this[--this.length];
+//             return lastItem;
+//         }
+//         return undefined;
+//     };
+
+//     this.unshift = function(newElem){
+//         this[0] = newElem;
+//         this.length++;
+//         return this.length;
+//     };//how to save the first item of the initial array?
+
+//     this.shift = function(){
+//         if(this.length > 0) {
+//             const firstItem = this[0];
+//             delete this[0];
+//             this.length--;
+//             return firstItem;
+//         }
+//         return undefined;
+//     };
+// }
+
+// MyArray.prototype = new MyArrayProto();
 
 const myArr1 = new MyArray();
 
